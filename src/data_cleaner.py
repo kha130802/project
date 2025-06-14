69c74d6c95c4d467698997ebfc0432c895b22ac2
Binh Le <letb1501@gmail.com>
2025-06-14 18:32:25 +0700

    chuyen doi kieu du lieu

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -15,3 +15,15 @@ class DataCleaner:
             self.df.dropna(subset=columns, inplace=True)
         else:
             self.df.dropna(inplace=True)
+    def convert_types(self, type_dict: dict):
+        """
+        Chuyển đổi kiểu dữ liệu của các cột trong DataFrame.
+        
+        Args:
+            type_dict (dict): Từ điển với tên cột là khóa và kiểu dữ liệu là giá trị.
+        """
+        for column, dtype in type_dict.items():
+            if column in self.df.columns:
+                self.df[column] = self.df[column].astype(dtype)
+            else:
+                print(f"Cột {column} không tồn tại trong DataFrame.")
