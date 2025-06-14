c9e3532fa59929f58a26de86168b53101df06a3d
Binh Le <letb1501@gmail.com>
2025-06-14 18:26:12 +0700

    Code null

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,23 +1,17 @@
-# data_cleaner.py
 import pandas as pd
 class DataCleaner:
     '''
         Lớp xử lý làm sạch, ép kiểu dữ liệu
-    '''
+''' 
     def __init__(self, df: pd.DataFrame):
         self.df = df
 
     def drop_nulls(self, columns: list = None):
-        pass
-
-    def convert_types(self, type_dict: dict):
-        pass
-
-    def normalize_column_names(self):
-        pass
-
-    def clean_text_columns(self, columns: list):
-        pass
-
-    def get_clean_data(self) -> pd.DataFrame:
-        return self.df
+        """
+        Xóa các hàng có giá trị null trong DataFrame.
+        Nếu columns được cung cấp, chỉ xóa các hàng có giá trị null trong các cột đó.
+        """
+        if columns:
+            self.df.dropna(subset=columns, inplace=True)
+        else:
+            self.df.dropna(inplace=True)
